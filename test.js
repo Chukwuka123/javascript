bba5223d3811c4048d8e5fc98928a37372ccab30
Chukky <user@Chukky.local>
2022-06-23 01:25:17 +0100

    first project push javascript

---
+++ test.js
@@ -0,0 +1,11 @@
+var name;
+name = prompt("what is your name?")
+document.write("name:" + name + "<br>")
+
+var height;
+height = prompt("what is your height?")
+document.write("Height:" + height + "<br>")
+
+var country;
+country = prompt("what country are you from?")
+document.write("country:" + country + "<br>")
